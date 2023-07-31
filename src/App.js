7434e48cf49a59d7dac6601dd244d7fbe6bb7909
w92120kd <kunj.dhola@student.manchester.ac.uk>
2023-07-31 10:37:35 +0530

    integrating redux

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import Header from './Header'
 import Sidebar from './Sidebar.js'
 import Feed from './Feed.js'
+import { useSelector } from 'react-redux';
+import {selectUser} from '../src/features/userSlice.js'
+import Login from '../src/Login'
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      {/* header  */}
       <Header/>
-      <div className="app__body">
+
+      {!user ? <Login/> : (<div className="app__body">
         <Sidebar/>
         <Feed/>
-      {/* main -> sidebar, feed, widgets */}
-      </div>
+      </div>)}
+      
       
     </div>
   );
