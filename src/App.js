0359d36c1e327b5995f311384554f3f09bd940a8
w92120kd <kunj.dhola@student.manchester.ac.uk>
2023-07-31 12:05:38 +0530

    added register feature

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Header from './Header'
 import Sidebar from './Sidebar.js'
 import Feed from './Feed.js'
 import { useSelector } from 'react-redux';
-import {selectUser} from '../src/features/userSlice.js'
+import { selectUser } from '../src/features/userSlice.js'
 import Login from '../src/Login'
 function App() {
   const user = useSelector(selectUser);
 
   return (
     <div className="app">
-      <Header/>
 
-      {!user ? <Login/> : (<div className="app__body">
-        <Sidebar/>
-        <Feed/>
-      </div>)}
-      
-      
+      {!user ? <Login /> : (<>     <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div></>)}
+
+
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,50 @@
+import '../src/Login.css'
+import { useState } from 'react';
+import {auth} from './firebase';
+import { useDispatch } from 'react-redux';
+import {login} from './features/userSlice'
 export default function Login(){
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+
+    const register = () => {
+        if(!name){
+            return alert("Please enter your full name to continue registering")
+        }
+        auth.createUserWithEmailAndPassword(email, password).then((userAuth)=>{
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: profilePic,
+            }).then(()=>{
+                dispatch(login(
+                    {
+                        email: userAuth.user.email,
+                            uid: userAuth.user.uid,
+                            displayName: name,
+                            photoUrl: profilePic,
+                    }
+                ))
+            })
+        }).catch((error) => alert(error));
+    };
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
     return (
-        <>
-        
-        </>
+        <div className="login">
+            <img src="https://logos-download.com/wp-content/uploads/2016/03/LinkedIn_Logo_2019.png" alt = "Linkedin logo"></img>
+            <form>
+                <input value={name} onChange = {(e) => setName(e.target.value)} placeholder = "Full name (required if registering)" type="text" />
+                <input value={profilePic} onChange = {(e) => setProfilePic(e.target.value)} placeholder = "Profile pic URL (optional)" type="text" />
+                <input value={email} onChange = {(e) => setEmail(e.target.value)} placeholder = "Email" type="email" />
+                <input value={password} onChange = {(e) => setPassword(e.target.value)} placeholder = "Password" type="password" />
+                <button type="submit" onClick={loginToApp}>Sign in</button>
+            </form>
+            <p>Not a member? {" "}<span className="login__register" onClick={register}>Register Now</span></p>
+        </div>
     );
 }
